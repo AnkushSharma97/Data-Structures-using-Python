4388c3d1875e64cff821cf7a07c9d5ebfd379047
AnkushSharma97 <69448944+AnkushSharma97@users.noreply.github.com>
2021-03-17 10:10:10 +0530

    Add files via upload

---
+++ DS-Tree/01 Recursive_BinaryTree_Traversals.py
@@ -0,0 +1,57 @@
+class Btnode:
+    def __init__(self,data):
+        self.left_child=None
+        self.info=data
+        self.right_child=None
+
+
+class BinaryTree:
+    
+    def preorder(self,root):
+        if root==None:
+            return
+        print(root.info,end=" ")
+        self.preorder(root.left_child)
+        self.preorder(root.right_child)
+
+    def inorder(self,root):
+        if root==None:
+            return
+        self.inorder(root.left_child)
+        print(root.info,end=" ")
+        self.inorder(root.right_child)
+
+    def postorder(self,root):
+        if root==None:
+            return
+        self.inorder(root.left_child)
+        self.inorder(root.right_child)
+        print(root.info,end=" ")
+
+    def levelorder(self,root):
+        from collections import deque #using internal deque or else we 
+        queu=deque()                  #can implement our own queue as well
+        while len(queu)!=0:
+            element=queu.popleft()
+            print(element.info,end=" ")
+            if element.left_child is not None:
+                queu.append(element.left_child)
+            if element.right_child is not None:
+                queu.append(element.right_child)
+'''here we will manually create the following tree using Btnode class:-
+           A
+          / \
+         B   C
+        / \
+       D   E
+'''
+tree=Btnode('A') #root node as tree
+tree.left_child=Btnode('B')
+tree.right_child=Btnode('C')
+tree.left_child.left_child=Btnode('D')
+tree.left_child.right_child=Btnode('E')
+
+
+bt=BinaryTree()
+print("traversal of given tree is --> ",end='')
+bt.postorder(tree)
---
+++ DS-Tree/02 Iterative_BinaryTree_Traversals.py
@@ -0,0 +1,105 @@
+#In recursive Travesral methds we use internal stack to implement the tree Traversals
+#but in Iterative method we  will use our own explicit Stack for the implementation
+#here we cant use static stack i.e. we need to use dynamic stack (the one created
+#using linked list) because we dont know how much size stack needs to store tree's
+#data in starting. or else we can use python list as well, as stack.
+class Btnode:
+    def __init__(self,data):
+        self.left_child=None
+        self.info=data
+        self.right_child=None
+
+class BinaryTree:
+    def preorder(self,root):
+        stack=[]
+        stack.append(root)
+        while len(stack)!=0:
+            data=stack.pop()
+            print(data.info,end=" ")
+            if data.right_child is not None:
+                stack.append(data.right_child)
+            if data.left_child is not None:
+                stack.append(data.left_child)
+    def inorder(self,root):
+        current=root
+        stack=[]
+        while True:
+            if current is not None:
+                stack.append(current)
+                current=current.left_child
+                #traversing till end of leftmost leaf
+            elif len(stack)!=0:
+                current=stack.pop()
+                print(current.info,end=" ")
+                current=current.right_child
+            else:
+                break
+
+    def postorder(self,root):
+        current=root
+        stack=[]
+        stack.append(current)
+        while len(stack)!=0:
+            if current is not None:
+                if current.right_child is not None:
+                    stack.append(current.right_child)
+                if current.left_child is not None:
+                    stack.append(current.left_child)
+                current=current.left_child
+                #traversing till end of leftmost leaf
+            elif len(stack)!=0:
+                current=stack.pop()
+                if current.left_child==None and current.right_child==None:
+                    last_visited=current
+                    print(current.info,end=" ")
+                    current=None #optional but if we use it it will not
+                                 #go to if loop unnecessarily.
+                elif current.right_child !=None:
+                    if current.right_child==last_visited:
+                        last_visited=current
+                        print(current.info,end=" ")
+                        current=None
+                    else:       
+                        stack.append(current)
+                elif current.left_child!=None:
+                    if current.left_child==last_visited:
+                            last_visited=current
+                            print(current.info,end=" ")
+                            current=None
+                    else:       
+                        stack.append(current)
+
+    def levelorder(self,root):
+        from collections import deque #using internal deque or else we 
+        queu=deque()                  #can implement our own queue as well
+        while len(queu)!=0:
+            element=queu.popleft()
+            print(element.info,end=" ")
+            if element.left_child is not None:
+                queu.append(element.left_child)
+            if element.right_child is not None:
+                queu.append(element.right_child)
+            
+            
+       
+'''here we will manually create the following tree using Btnode class:-
+           A
+          / \
+         B   C
+        / \
+       D   E
+          / \
+         F   G
+'''
+root=Btnode('A') #root node as root
+root.left_child=Btnode('B')
+#root.right_child=Btnode('C')
+root.left_child.left_child=Btnode('D')
+root.left_child.right_child=Btnode('E')
+root.left_child.right_child.left_child=Btnode('F')
+root.left_child.right_child.right_child=Btnode('G')
+
+
+bt=BinaryTree()
+print("traversal of given tree is --> ",end='')
+bt.levelorder(root)
